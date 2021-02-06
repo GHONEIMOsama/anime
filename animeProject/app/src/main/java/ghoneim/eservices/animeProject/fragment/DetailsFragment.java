3c028ba25bb34e51de00d27f2d29ba412a247128
ghoneim <osama.ghoneim.etu@univ-lille.fr>
2021-02-06 12:58:42 +0100

    CHORE: implement the design for details fragment

--- animeProject/app/src/main/java/ghoneim/eservices/animeProject/fragment/DetailsFragment.java
+++ animeProject/app/src/main/java/ghoneim/eservices/animeProject/fragment/DetailsFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import ghoneim.eservices.animeProject.R;
 
 public class DetailsFragment extends Fragment {
+
+    private ImageView imageView;
+    private TextView titleTextView;
+    private TextView descriptionTextView;
 
     public DetailsFragment() {}
 
@@ -21,6 +27,9 @@ public class DetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_details, container, false);
+        imageView = (ImageView) v.findViewById(R.id.anime_image);
+        titleTextView = (TextView) v.findViewById(R.id.anime_title);
+        descriptionTextView = (TextView) v.findViewById(R.id.anime_description);
         return v;
     }
 }
